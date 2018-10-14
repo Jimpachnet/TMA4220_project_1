abde66987548fa7cb80b125d5a747c1aeab9b9ad
Leon Sievers <leon.sievers@tum.de>
2018-10-14 12:55:26 +0200

    Added setup

---
+++ project_1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Project_1',
+      version='1.0',
+      description='Our solution to project 1 of the course TMA4220 in autumn semester 2018 at NTNU Trondheim',
+      author='Leon Sievers, Thibault Gaudet',
+      author_email='leon.sievers@tum.de',
+      url='https://github.com/Jimpachnet/TMA4220_project_1',
+      packages=['distutils', 'distutils.command'],
+     )
