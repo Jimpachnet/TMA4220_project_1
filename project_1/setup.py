076ed8eabdd812623925c0f514896d56b33a932a
Leon Sievers <leon.sievers@tum.de>
2018-10-14 12:57:37 +0200

    Created package

--- project_1/setup.py
+++ project_1/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='Project_1',
-      version='1.0',
+      version='0.1dev',
       description='Our solution to project 1 of the course TMA4220 in autumn semester 2018 at NTNU Trondheim',
       author='Leon Sievers, Thibault Gaudet',
       author_email='leon.sievers@tum.de',
